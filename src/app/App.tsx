4c2cd0db0646ad347217e2d43c55bb68a782db9e
Volodymyr_Pavlov <Volodymyr_Pavlov>
2025-01-09 11:37:27 +0200

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='font-bold text-center text-teal-700 text-5xl'>Library App</h1>
+    </>
+  )
+}
+
+export default App
